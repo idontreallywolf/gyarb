b871df1722359f48db85b11a9cd5c7b68e8e9ab4
Nedim Kanat <nedim.kanat1999@gmail.com>
2018-10-09 08:55:36 +0200

    .-.

---
+++ bin/tiledmap/newLevel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset" tilewidth="32" tileheight="32" tilecount="322" columns="23">
+ <image source="../Tileset.png" width="752" height="448"/>
+</tileset>
