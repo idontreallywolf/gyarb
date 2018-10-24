fbef3d14d7e428c7e0ffa4d4fb8ec79de71c2405
nedimkanat <nedim.kanat1999@gmail.com>
2018-10-24 09:04:13 +0200

    raycast

--- src/pleb/Player.java
+++ src/pleb/Player.java
@@ -6,6 +6,7 @@ import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Vector;
 
 public class Player extends GameObject {
 
@@ -23,6 +24,7 @@ public class Player extends GameObject {
 	private ParticleManager particleMgr;
 	private ArrayList<Bullet> Bullets = new ArrayList<Bullet>();
 	private ArrayList<Bullet> removeBullets = new ArrayList<Bullet>();
+	private Vector<Float> rayv = new Vector<Float>(2);
 	private static double timeElapsed;
 
 	public Rectangle bounds, boundsTop, boundsBottom, boundsLeft, boundsRight;
@@ -49,6 +51,7 @@ public class Player extends GameObject {
 
 		particleMgr = new ParticleManager();
 		bounds = new Rectangle( (int)x, (int)y, (int)width, (int)height );
+
 	}
 
 
@@ -65,7 +68,8 @@ public class Player extends GameObject {
 				setVelY(Y_MAX_SPEED);
 		}
 
-
+		
+		
 		CollisionAndCondition(object);
 
 		if(isBouncing) {
@@ -80,6 +84,8 @@ public class Player extends GameObject {
 		particleMgr.update(time);
 
 		handleBullets(this.direction, object, dt, time);
+		
+		
 	}
 
 	public void render(Graphics2D g, float dt) {
@@ -90,15 +96,101 @@ public class Player extends GameObject {
 
 		g.setColor(getSelfColor());
 		g.fillRect((int)getX(), (int)getY(), (int)getWidth(), (int)getHeight());
-
-		g.setColor(Color.RED);
+		
+		raycast(g);
+		/*
+		g.setColor(Color.WHITE);
+		g.draw(getBounds());
 		g.draw(getBoundsTop());
 		g.draw(getBoundsBottom());
 		g.draw(getBoundsLeft());
 		g.draw(getBoundsRight());
-
+		*/
 		showHP(g);
 
+	}
+	
+	boolean raycast(GameObject ent, float startx, float starty, float directionx, float directiony, float length, LinkedList<GameObject> mask, Graphics2D g) {
+	    return raycast(ent, startx, starty, startx + normalized(directionx, directiony)[0] * length, starty + normalized(directionx, directiony)[1] * length, mask, 1.0f, g);
+	}
+	 
+	boolean raycast(GameObject ent, float startx, float starty, float directionx, float directiony, float length, LinkedList<GameObject> mask, float precision, Graphics2D g) {
+	    return raycast(ent, startx, starty, startx + normalized(directionx, directiony)[0] * length, starty + normalized(directionx, directiony)[1] * length, mask, precision, g);
+	}
+	 
+	boolean raycast(GameObject ent, float startx, float starty, float endx, float endy, LinkedList<GameObject> mask, Graphics2D g) {
+	    return raycast(ent, startx, starty, endx, endy, mask, 1.0f, g);
+	}
+	 
+	boolean raycast(GameObject ent, float startx, float starty, float endx, float endy, LinkedList<GameObject> mask, float precision, Graphics2D g) {
+	    float currentx = startx;
+	    float currenty = starty;
+	    
+	    while (Math.abs(endx - currentx) + Math.abs(endy - currenty) >= precision) {
+	        currentx -= normalized(startx - endx, starty - endy)[0] * precision;
+	        currenty -= normalized(startx - endx, starty - endy)[1] * precision;
+
+	        Rectangle c = new Rectangle((int)currentx, (int)currenty, (int)precision, (int)precision);
+	        
+	        //g.setColor(Color.PINK);
+	        //g.drawRect((int)currentx, (int)currenty, (int)Math.ceil(precision), (int)Math.ceil(precision));
+	        
+	        for (GameObject go : mask) {
+	        	
+	        	if (go == ent)
+	        		continue;
+	        	
+	        	if (c.getBounds().intersects(go.getBounds())) {
+	        		this.rayv.add(0, go.getX());
+	        		this.rayv.add(1, go.getY());
+	        		return true;
+	        	}
+			}
+	    }
+	   
+	    return false;
+	}
+	
+	private float mag(float x, float y) {
+		return (float) Math.sqrt((double)(x * x + y * y));
+	} 
+	
+	private float[] normalized(float x, float y) {
+		float m = mag(x, y);
+		float[] f = {x, y};
+		if (m > 0) {
+			f[0] = f[0] / m;
+			f[1] = f[1] / m;
+		}
+	    return f;
+	}
+	
+	private void raycast(Graphics2D g) {		
+		// Left
+		if (raycast(this, getX(), getY(), -1, 0f, 100, Game.objects, 1.0f, g)) 
+		{  
+			//System.out.println(getX()+":"+this.rayv.get(0));
+			if(getX() <= this.rayv.get(0)+Config.General.tilesize) {
+				setX(this.rayv.get(0)+Config.General.tilesize);
+				setVelX(0);	
+			}
+		}
+		
+		// Right
+		if (raycast(this, getX(), getY(), 1, 0f, 132, Game.objects, 1.0f, g)) 
+		{ 
+			//System.out.println(getX()+":"+this.rayv.get(0));
+			if(getX()+Config.General.tilesize >= this.rayv.get(0)) {
+				setX(this.rayv.get(0)-Config.General.tilesize);
+				setVelX(0);
+				
+			}
+		}
+		
+		
+		// Down
+		/*if (raycast(this, getX(), getY(), 0, 1f, 100, Game.objects, 1.0f, g)) 
+		{ }*/
 	}
 
 
@@ -189,6 +281,8 @@ public class Player extends GameObject {
 				continue;
 			
 			
+			
+			
 			if(tempObject.getId() == ObjectId.Block) {
 
 				if(getBoundsTop().intersects(tempObject.getBoundsBottom())){
@@ -204,19 +298,20 @@ public class Player extends GameObject {
 					isBouncing = false;
 				} else { falling = true; }
 
-				if(getBoundsRight().intersects(tempObject.getBoundsLeft())){
+				
+				/*if(getBoundsRight().intersects(tempObject.getBoundsLeft())){
 					intersectsRight = true;
 					x = tempObject.getX() - this.width;
 					setVelX(0);
 
-				} else { intersectsRight = false; }
+				} else { intersectsRight = false; }*/
 
 
-				if(getBoundsLeft().intersects(tempObject.getBoundsRight())){
+				/*if(getBoundsLeft().intersects(tempObject.getBoundsRight())){
 					intersectsLeft = true;
 					x = tempObject.getX() + this.width;
 					setVelX(0);
-				} else { intersectsLeft = false; }
+				} else { intersectsLeft = false; }*/
 
 			} else if (tempObject.getId() == ObjectId.Obstacle) {
 
